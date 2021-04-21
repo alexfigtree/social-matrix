4a9f44452c56a2db1ec061ccff5405d37ed822a4
Alex <alexandra.higuera@gmail.com>
2021-04-21 18:24:53 -0400

    Rename axes combining for 2 domainse

--- client/src/components/Form/QuestionData.js
+++ client/src/components/Form/QuestionData.js
@@ -2,37 +2,34 @@ export let questionData = [
   // Subject 1 (JK) - Pre/Earlier
   //dark orange, smaller values
   [
-    {axis:"Educ. Level","value":0},
-    {axis:"Unique History",value:0},
-    {axis:"Sex Assigned at Birth",value:0},
-    {axis:"Gender Expression",value:0},
-    {axis:"Gender ID",value:0},
-    {axis:"Sexual Orientation",value:0},
-    {axis:"Marital Status",value:0},
+    {axis:"Educ. Level","value":0}, //q1
+    {axis:"Unique History",value:0}, //q34
+    {axis:"Sex Assigned at Birth",value:0}, //q33
+    {axis:"Gender Expression",value:0}, //q32
+    {axis:"Gender ID",value:0}, //q31
+    {axis:"Sexual Orientation",value:0}, //q30
+    {axis:"Marital Status",value:0}, //q29
 
-    {axis:"Parental Status",value:0}, 
-    {axis:"Guardians’ Marital Status",value:0},
-    {axis:"Raised By",value:0},
-    {axis:"Age",value:0},
-    {axis:"Appearance",value:0},
-    {axis:"Health/Ability",value:0},
+    {axis:"Parental Status",value:0}, //q28
+    {axis:"Guardians’ Marital Status",value:0}, //q27
+    {axis:"Raised By",value:0}, //q26
+    {axis:"Age",value:0}, //q25
+    {axis:"Appearance",value:0}, //q24
+    {axis:"Health/Ability",value:0}, //q23
 
-    {axis:"Cognitive Ability/Style",value:0},
-    {axis:"MH/Subst. Abuse",value:0}, //take away
-    {axis:"Relation to MH System",value:0}, //change to MH/Substance Use (come with above)
-    {axis:"Relation to Legal System",value:0}, 
-    {axis:"Relation to Military",value:0},
-    {axis:"Impact of Historical Times",value:0},
+    {axis:"Cognitive Ability/Style",value:0}, //q22
+    {axis:"MH/Substance Use",value:0}, //q21
+    {axis:"Relation to Legal System",value:0}, //q20
+    {axis:"Relation to Military",value:0}, //q19
+    {axis:"Impact of Historical Times",value:0}, //q18
 
-    {axis:"Geo. Region",value:0},
-    {axis:"Politics",value:0},
-    {axis:"Chosen Religion",value:0},
-    {axis:"Family Religion",value:0},
-    {axis:"Language(s) Spoken",value:0},
-    {axis:"# Generations in Country",value:0}, //take away
+    {axis:"Geo. Region",value:0}, //q17
+    {axis:"Politics",value:0}, //q16
+    {axis:"Chosen Religion",value:0}, //q15
+    {axis:"Family Religion",value:0}, //q14
+    {axis:"Language(s) Spoken",value:0}, //q13
 
-    {axis:"Citizenship Status",value:0}, //take away
-    {axis:"Person's Nation of Origin",value:0}, //q12
+    {axis:"Nation of Origin / Citizenship",value:0}, //q12
     {axis:"Ethnicity / Tribe",value:0}, //q11
     {axis:"Race(s)",value:0}, //q10
     {axis:"Guardians’ Access",value:0}, //q9
@@ -47,139 +44,130 @@ export let questionData = [
   ],
     //light orange, larger values
   [
-    {axis:"Educ. Level","value":0},
-    {axis:"Unique History",value:0},
-    {axis:"Sex Assigned at Birth",value:0},
-    {axis:"Gender Expression",value:0},
-    {axis:"Gender ID",value:0},
-    {axis:"Sexual Orientation",value:0},
-    {axis:"Marital Status",value:0},
+    {axis:"Educ. Level","value":0}, //q1
+    {axis:"Unique History",value:0}, //q34
+    {axis:"Sex Assigned at Birth",value:0}, //q33
+    {axis:"Gender Expression",value:0}, //q32
+    {axis:"Gender ID",value:0}, //q31
+    {axis:"Sexual Orientation",value:0}, //q30
+    {axis:"Marital Status",value:0}, //q29
 
-    {axis:"Parental Status",value:0}, 
-    {axis:"Guardians’ Marital Status",value:0},
-    {axis:"Raised By",value:0},
-    {axis:"Age",value:0},
-    {axis:"Appearance",value:0},
-    {axis:"Health/Ability",value:0},
+    {axis:"Parental Status",value:0}, //q28
+    {axis:"Guardians’ Marital Status",value:0}, //q27
+    {axis:"Raised By",value:0}, //q26
+    {axis:"Age",value:0}, //q25
+    {axis:"Appearance",value:0}, //q24
+    {axis:"Health/Ability",value:0}, //q23
 
-    {axis:"Cognitive Ability/Style",value:0},
-    {axis:"MH/Subst. Abuse",value:0},
-    {axis:"Relation to MH System",value:0},
-    {axis:"Relation to Legal System",value:0},
-    {axis:"Relation to Military",value:0},
-    {axis:"Impact of Historical Times",value:0},
+    {axis:"Cognitive Ability/Style",value:0}, //q22
+    {axis:"MH/Substance Use",value:0}, //q21
+    {axis:"Relation to Legal System",value:0}, //q20
+    {axis:"Relation to Military",value:0}, //q19
+    {axis:"Impact of Historical Times",value:0}, //q18
 
-    {axis:"Geo. Region",value:0},
-    {axis:"Politics",value:0},
-    {axis:"Chosen Religion",value:0},
-    {axis:"Family Religion",value:0},
-    {axis:"Language(s) Spoken",value:0},
-    {axis:"# Generations in Country",value:0},
+    {axis:"Geo. Region",value:0}, //q17
+    {axis:"Politics",value:0}, //q16
+    {axis:"Chosen Religion",value:0}, //q15
+    {axis:"Family Religion",value:0}, //q14
+    {axis:"Language(s) Spoken",value:0}, //q13
 
-    {axis:"Citizenship Status",value:0},
-    {axis:"Person's Nation of Origin",value:0},
-    {axis:"Ethnicity / Tribe",value:0},
-    {axis:"Race(s)",value:0},
-    {axis:"Guardians’ Access",value:0},
-    {axis:"Access",value:0},
+    {axis:"Nation of Origin / Citizenship",value:0}, //q12
+    {axis:"Ethnicity / Tribe",value:0}, //q11
+    {axis:"Race(s)",value:0}, //q10
+    {axis:"Guardians’ Access",value:0}, //q9
+    {axis:"Access",value:0}, //q8
 
-    {axis:"Guardian’s Childhood House. Income",value:0},
-    {axis:"Childhood House. Income w. Guardian",value:0},
-    {axis:"Household Income (Now)",value:0},
-    {axis:"Guardians’ Occupation",value:0},
-    {axis:"Occupation(s)",value:0},
-    {axis:"Guardians’ Educ.",value:0}   
+    {axis:"Guardian’s Childhood House. Income",value:0}, //q7
+    {axis:"Childhood House. Income w. Guardian",value:0}, //q6
+    {axis:"Household Income (Now)",value:0}, //q5
+    {axis:"Guardians’ Occupation",value:0}, //q4
+    {axis:"Occupation(s)",value:0}, //q3
+    {axis:"Guardians’ Educ.",value:0} //q2 
   ],
   // Subject 1 (JK) - Post/Current
   //dark blue, smaller values
   [
-    {axis:"Educ. Level","value":0},
-    {axis:"Unique History",value:0},
-    {axis:"Sex Assigned at Birth",value:0},
-    {axis:"Gender Expression",value:0},
-    {axis:"Gender ID",value:0},
-    {axis:"Sexual Orientation",value:0},
-    {axis:"Marital Status",value:0},
+    {axis:"Educ. Level","value":0}, //q1
+    {axis:"Unique History",value:0}, //q34
+    {axis:"Sex Assigned at Birth",value:0}, //q33
+    {axis:"Gender Expression",value:0}, //q32
+    {axis:"Gender ID",value:0}, //q31
+    {axis:"Sexual Orientation",value:0}, //q30
+    {axis:"Marital Status",value:0}, //q29
 
-    {axis:"Parental Status",value:0}, 
-    {axis:"Guardians’ Marital Status",value:0},
-    {axis:"Raised By",value:0},
-    {axis:"Age",value:0},
-    {axis:"Appearance",value:0},
-    {axis:"Health/Ability",value:0},
+    {axis:"Parental Status",value:0}, //q28
+    {axis:"Guardians’ Marital Status",value:0}, //q27
+    {axis:"Raised By",value:0}, //q26
+    {axis:"Age",value:0}, //q25
+    {axis:"Appearance",value:0}, //q24
+    {axis:"Health/Ability",value:0}, //q23
 
-    {axis:"Cognitive Ability/Style",value:0},
-    {axis:"MH/Subst. Abuse",value:0},
-    {axis:"Relation to MH System",value:0},
-    {axis:"Relation to Legal System",value:0},
-    {axis:"Relation to Military",value:0},
-    {axis:"Impact of Historical Times",value:0},
+    {axis:"Cognitive Ability/Style",value:0}, //q22
+    {axis:"MH/Substance Use",value:0}, //q21
+    {axis:"Relation to Legal System",value:0}, //q20
+    {axis:"Relation to Military",value:0}, //q19
+    {axis:"Impact of Historical Times",value:0}, //q18
 
-    {axis:"Geo. Region",value:0},
-    {axis:"Politics",value:0},
-    {axis:"Chosen Religion",value:0},
-    {axis:"Family Religion",value:0},
-    {axis:"Language(s) Spoken",value:0},
-    {axis:"# Generations in Country",value:0},
+    {axis:"Geo. Region",value:0}, //q17
+    {axis:"Politics",value:0}, //q16
+    {axis:"Chosen Religion",value:0}, //q15
+    {axis:"Family Religion",value:0}, //q14
+    {axis:"Language(s) Spoken",value:0}, //q13
 
-    {axis:"Citizenship Status",value:0},
-    {axis:"Person's Nation of Origin",value:0},
-    {axis:"Ethnicity / Tribe",value:0},
-    {axis:"Race(s)",value:0},
-    {axis:"Guardians’ Access",value:0},
-    {axis:"Access",value:0},
+    {axis:"Nation of Origin / Citizenship",value:0}, //q12
+    {axis:"Ethnicity / Tribe",value:0}, //q11
+    {axis:"Race(s)",value:0}, //q10
+    {axis:"Guardians’ Access",value:0}, //q9
+    {axis:"Access",value:0}, //q8
 
-    {axis:"Guardian’s Childhood House. Income",value:0},
-    {axis:"Childhood House. Income w. Guardian",value:0},
-    {axis:"Household Income (Now)",value:0},
-    {axis:"Guardians’ Occupation",value:0},
-    {axis:"Occupation(s)",value:0},
-    {axis:"Guardians’ Educ.",value:0} 
+    {axis:"Guardian’s Childhood House. Income",value:0}, //q7
+    {axis:"Childhood House. Income w. Guardian",value:0}, //q6
+    {axis:"Household Income (Now)",value:0}, //q5
+    {axis:"Guardians’ Occupation",value:0}, //q4
+    {axis:"Occupation(s)",value:0}, //q3
+    {axis:"Guardians’ Educ.",value:0} //q2
   ],
   //light blue, larger values
   [
-    {axis:"Educ. Level","value":0},
-    {axis:"Unique History",value:0},
-    {axis:"Sex Assigned at Birth",value:0},
-    {axis:"Gender Expression",value:0},
-    {axis:"Gender ID",value:0},
-    {axis:"Sexual Orientation",value:0},
-    {axis:"Marital Status",value:0},
+    {axis:"Educ. Level","value":0}, //q1
+    {axis:"Unique History",value:0}, //q34
+    {axis:"Sex Assigned at Birth",value:0}, //q33
+    {axis:"Gender Expression",value:0}, //q32
+    {axis:"Gender ID",value:0}, //q31
+    {axis:"Sexual Orientation",value:0}, //q30
+    {axis:"Marital Status",value:0}, //q29
 
-    {axis:"Parental Status",value:0}, 
-    {axis:"Guardians’ Marital Status",value:0},
-    {axis:"Raised By",value:0},
-    {axis:"Age",value:0},
-    {axis:"Appearance",value:0},
-    {axis:"Health/Ability",value:0},
+    {axis:"Parental Status",value:0}, //q28
+    {axis:"Guardians’ Marital Status",value:0}, //q27
+    {axis:"Raised By",value:0}, //q26
+    {axis:"Age",value:0}, //q25
+    {axis:"Appearance",value:0}, //q24
+    {axis:"Health/Ability",value:0}, //q23
 
-    {axis:"Cognitive Ability/Style",value:0},
-    {axis:"MH/Subst. Abuse",value:0},
-    {axis:"Relation to MH System",value:0},
-    {axis:"Relation to Legal System",value:0},
-    {axis:"Relation to Military",value:0},
-    {axis:"Impact of Historical Times",value:0},
+    {axis:"Cognitive Ability/Style",value:0}, //q22
+    {axis:"MH/Substance Use",value:0}, //q21
+    {axis:"Relation to Legal System",value:0}, //q20
+    {axis:"Relation to Military",value:0}, //q19
+    {axis:"Impact of Historical Times",value:0}, //q18
 
-    {axis:"Geo. Region",value:0},
-    {axis:"Politics",value:0},
-    {axis:"Chosen Religion",value:0},
-    {axis:"Family Religion",value:0},
-    {axis:"Language(s) Spoken",value:0},
-    {axis:"# Generations in Country",value:0},
+    {axis:"Geo. Region",value:0}, //q17
+    {axis:"Politics",value:0}, //q16
+    {axis:"Chosen Religion",value:0}, //q15
+    {axis:"Family Religion",value:0}, //q14
+    {axis:"Language(s) Spoken",value:0}, //q13
 
-    {axis:"Citizenship Status",value:0},
-    {axis:"Person's Nation of Origin",value:0},
-    {axis:"Ethnicity / Tribe",value:0},
-    {axis:"Race(s)",value:0},
-    {axis:"Guardians’ Access",value:0},
-    {axis:"Access",value:0},
+    {axis:"Nation of Origin / Citizenship",value:0}, //q12
+    {axis:"Ethnicity / Tribe",value:0}, //q11
+    {axis:"Race(s)",value:0}, //q10
+    {axis:"Guardians’ Access",value:0}, //q9
+    {axis:"Access",value:0}, //q8
 
-    {axis:"Guardian’s Childhood House. Income",value:0},
-    {axis:"Childhood House. Income w. Guardian",value:0},
-    {axis:"Household Income (Now)",value:0},
-    {axis:"Guardians’ Occupation",value:0},
-    {axis:"Occupation(s)",value:0},
-    {axis:"Guardians’ Educ.",value:0}  
+    {axis:"Guardian’s Childhood House. Income",value:0}, //q7
+    {axis:"Childhood House. Income w. Guardian",value:0}, //q6
+    {axis:"Household Income (Now)",value:0}, //q5
+    {axis:"Guardians’ Occupation",value:0}, //q4
+    {axis:"Occupation(s)",value:0}, //q3
+    {axis:"Guardians’ Educ.",value:0} //q2
   ]
 ];
 
