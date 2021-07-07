272a7ac7bab6ed89d85c48eca3874af384de98d8
Alex <alexandra.higuera@gmail.com>
2021-07-06 21:59:39 -0400

    Add to App.js, update packages

--- client/src/App.js
+++ client/src/App.js
@@ -85,6 +85,19 @@ import UniqueHistory from './components/Questions/UniqueHistory';
 
 import Results from './components/Results/Results';
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 const App = () => {
   return (
