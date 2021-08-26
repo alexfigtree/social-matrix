14546763b1b19be27ff15f8ba1e810cf8842009f
Alex <alexandra.higuera@gmail.com>
2021-08-26 18:00:02 -0400

    Remove new package test

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -8,7 +8,7 @@ import * as data from '../../data/dataBlank.js';
 
 import useStyles from '../../styles';
 
-import html2pdf from 'html2pdf.js';
+//import html2pdf from 'html2pdf.js';
 
 import { getMatrix, createMatrix, updateMatrix, createFootnotes, getFootnotes, updateFootnotes } from '../../actions/posts';
 
@@ -619,7 +619,7 @@ const Results = () => {
         filename: 'my-social-matrix.pdf',
       };
 
-      html2pdf().set(opt).from(element).save();
+      //html2pdf().set(opt).from(element).save();
     };
 
   return (
