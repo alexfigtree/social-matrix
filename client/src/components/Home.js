01d3d90809a16656adcd8abf4174a6ab56211868
Alex <alexandra.higuera@gmail.com>
2021-09-04 15:05:14 -0400

    New HashLink Library test

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,6 +27,7 @@ import NavBar from './NavBar.js';
 import useStyles from '../styles';
 
 import styles from '../style.css';
+import { HashLink } from 'react-router-hash-link';
 
 const Home = () => {
     let tempData = data;
@@ -571,7 +572,7 @@ const Home = () => {
 
             <Typography variant="h6" align="left" style={{ marginLeft: '10%', marginRight: '10%', paddingBottom: '20px', paddingTop: '20px'}}>
                 The Social Matrix Diagram includes many different&nbsp; 
-                <Link style={{ textDecoration: 'none', fontSize: '20px'}} to="/identity#householdIncome">areas of identity</Link>
+                <HashLink style={{ textDecoration: 'none', fontSize: '20px'}} to="/identity#householdIncome">areas of identity</HashLink>
                 , like educational level, income level, race, ethnicity, religion, sexual orientation, etc. Each aspect of 
                 identity falls along a line (or axis) going from the most highly privileged “1” at the center 
                  the diagram to the most marginalized “7” at the outermost circle. 
